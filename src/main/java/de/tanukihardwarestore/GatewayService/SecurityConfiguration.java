a7e6d2d655c5312004d388504c31a95f886c4ede
Karl Blume <profanta@schlaptop>
2022-07-17 11:50:43 +0200

    changed docker networking parameters

--- src/main/java/de/tanukihardwarestore/GatewayService/SecurityConfiguration.java
+++ src/main/java/de/tanukihardwarestore/GatewayService/SecurityConfiguration.java
@@ -1,10 +1,30 @@
 package de.tanukihardwarestore.GatewayService;
 
 import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.server.session.WebSessionManager;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @EnableWebFluxSecurity
 public class SecurityConfiguration {
@@ -12,24 +32,69 @@ public class SecurityConfiguration {
     private final WebEndpointProperties webEndpointProperties;
 
     public SecurityConfiguration(
-            WebEndpointProperties webEndpointProperties) {
+            WebEndpointProperties webEndpointProperties, @Lazy CorsFilter corsFilter) {
         this.webEndpointProperties = webEndpointProperties;
+        this.corsFilter = corsFilter;
     }
+
+    private final CorsFilter corsFilter;
+
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
 
         http
+                .cors().and()
+                .csrf().disable()
+
                 .authorizeExchange()
                 .pathMatchers("/actuator/**")
                 .permitAll()
                 .and()
+
                 .authorizeExchange()
                 .anyExchange()
                 .authenticated()
                 .and()
+
                 .oauth2ResourceServer()
                 .jwt();
+
         return http.build();
+    }
+
+
+
+    /*@Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowCredentials(false);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }*/
+
+    @Bean
+    public WebSessionManager webSessionManager() {
+        // Emulate SessionCreationPolicy.STATELESS
+        return exchange -> Mono.empty();
+    }
+    @Bean
+    public WebMvcConfigurer corsConfiguration() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods(HttpMethod.GET.name(), HttpMethod.OPTIONS.name(), HttpMethod.POST.name())
+                        .allowedOrigins("http://localhost:9000");
+            }
+        };
     }
 }
